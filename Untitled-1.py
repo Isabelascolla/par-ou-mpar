68430e9ce6bd918e59fd716a2ab81a6f12438f52
Isabelascolla <102077830+Isabelascolla@users.noreply.github.com>
2022-12-15 19:37:38 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,9 @@
+#programa que identifica se determinado número é par ou ímpar
+
+num = int(input())
+#numero inteiro: o resto da divisão = 0
+
+if (num%2) == 0:
+    print("o número é par")
+else:
+    print("o número é ímpar")
